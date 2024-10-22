ff203fe3c02fd96b62368b45c7db5479d9d4ffae
fayyozbobokulov <b.fayyoz2000@gmail.com>
2024-10-23 02:18:48 +0500

    fix: configured org module to properly connect with the app

--- src/modules/organization/organization.module.ts
+++ src/modules/organization/organization.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrganizationService } from './organization.service';
 import { OrganizationController } from './organization.controller';
+import { OrganizationRepository } from './organization.repository';
+import { KnexModule } from '../knex/knex.module';
 
 @Module({
-  providers: [OrganizationService],
+  imports: [KnexModule],
+  providers: [OrganizationService, OrganizationRepository],
   controllers: [OrganizationController],
 })
 export class OrganizationModule {}
