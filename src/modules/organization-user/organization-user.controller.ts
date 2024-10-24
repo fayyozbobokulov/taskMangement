e1b677ab40e472f40edbffe2574c5ec7bbc0c1a9
fayyozbobokulov <b.fayyoz2000@gmail.com>
2024-10-24 08:22:06 +0500

    fix: added role based access to organization-user

--- src/modules/organization-user/organization-user.controller.ts
+++ src/modules/organization-user/organization-user.controller.ts
@@ -7,17 +7,23 @@ import {
   Param,
   ParseIntPipe,
   Request,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateOrganizationUserDto } from './dto/create-organization-user';
 import { OrganizationUserService } from './organization-user.service';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserRoles } from '../user/interface/user-role.enum';
 
 @Controller('organization-users')
+@UseGuards(RolesGuard)
 export class OrganizationUserController {
   constructor(
     private readonly organizationUserService: OrganizationUserService,
   ) {}
 
   @Post()
+  @Roles(UserRoles.Admin)
   async addUserToOrganization(
     @Request() req,
     @Body() createOrgUserDto: CreateOrganizationUserDto,
@@ -29,6 +35,7 @@ export class OrganizationUserController {
   }
 
   @Delete(':orgId/users/:userId')
+  @Roles(UserRoles.Admin)
   async removeUserFromOrganization(
     @Request() req,
     @Param('orgId', ParseIntPipe) orgId: number,
@@ -42,11 +49,13 @@ export class OrganizationUserController {
   }
 
   @Get('user/:userId/organizations')
+  @Roles(UserRoles.Admin)
   async getUserOrganizations(@Param('userId', ParseIntPipe) userId: number) {
     return this.organizationUserService.getUserOrganizations(userId);
   }
 
   @Get('organization/:orgId/users')
+  @Roles(UserRoles.Admin)
   async getOrganizationUsers(
     @Request() req,
     @Param('orgId', ParseIntPipe) orgId: number,
