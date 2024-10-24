9d43b6ee4b2c44d00cb165ac58d6e2b3b1fcd650
fayyozbobokulov <b.fayyoz2000@gmail.com>
2024-10-24 05:04:28 +0500

    fix: added org-user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrganizationModule } from './modules/organization/organization.module';
 import { UsersModule } from './modules/user/user.module';
+import { OrganizationUsersModule } from './modules/organization-user/organization-user.module';
 
 @Module({
-  imports: [OrganizationModule, UsersModule],
+  imports: [OrganizationModule, UsersModule, OrganizationUsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
