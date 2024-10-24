6423f30134465030f7990c99d5a00af5128145e3
fayyozbobokulov <b.fayyoz2000@gmail.com>
2024-10-24 09:25:34 +0500

    fix :added proper imports to attach the modules

--- src/modules/statistic/statistic.module.ts
+++ src/modules/statistic/statistic.module.ts
@@ -3,10 +3,12 @@ import { ProjectsModule } from '../project/project.module';
 import { UsersModule } from '../user/user.module';
 import { StatisticsService } from './statistic.service';
 import { OrganizationsModule } from '../organization/organization.module';
+import { StatisticsController } from './statistic.controller';
+import { TasksModule } from '../task/task.module';
 
 @Module({
-  imports: [ProjectsModule, UsersModule, OrganizationsModule],
-  controllers: [StatisticsModule],
+  imports: [ProjectsModule, UsersModule, OrganizationsModule, TasksModule],
+  controllers: [StatisticsController],
   providers: [StatisticsService],
 })
 export class StatisticsModule {}
